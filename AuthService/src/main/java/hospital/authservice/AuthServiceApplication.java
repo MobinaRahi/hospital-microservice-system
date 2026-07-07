62b4964dbc5959881056a1ae564157a28d855cbb
mobinaRahi <marinacoda21@gmail.com>
2026-07-07 21:24:27 +0330

    add AuthService

--- AuthService/src/main/java/hospital/authservice/AuthServiceApplication.java
+++ AuthService/src/main/java/hospital/authservice/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.hospital.authservice;
+package hospital.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
