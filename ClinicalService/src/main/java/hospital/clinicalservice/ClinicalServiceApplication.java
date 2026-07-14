3b2976007102e49785b9266ffe669e9887236156
mobinaRahi <marinacoda21@gmail.com>
2026-07-14 21:39:57 +0330

    add ClinicalService

---
+++ ClinicalService/src/main/java/hospital/clinicalservice/ClinicalServiceApplication.java
@@ -0,0 +1,13 @@
+package hospital.clinicalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalServiceApplication.class, args);
+    }
+
+}
