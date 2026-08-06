3f64c4e8c27f45abcf8882e34fb4e81b499efc12
mobinaRahi <marinacoda21@gmail.com>
2026-08-06 14:49:53 +0330

    add inventoryService edited

--- InventoryService/src/main/java/hospital/inventoryservice/InventoryServiceApplication.java
+++ InventoryService/src/main/java/hospital/inventoryservice/InventoryServiceApplication.java
@@ -2,12 +2,26 @@ package hospital.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * InventoryService Application Entry Point
+ *
+ * Manages:
+ * - Drugs and drug categories
+ * - Stock management
+ * - Suppliers
+ * - Purchase orders
+ * - Equipment and equipment assignments
+ *
+ * @author MobinaRahi
+ */
 @SpringBootApplication
+@EnableJpaAuditing
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
 
 }
